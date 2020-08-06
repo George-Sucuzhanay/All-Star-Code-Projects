93ad40a2d85690e52d4fe08184f4a1feba9b3d68
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-08-06 03:23:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-// get references
-let colorDiv = document.getElementById("color_div");
-let body = document.querySelector("body");
+// get reference for the button
+let button = document.querySelector("button");
 
-// make a changeColor function
-function changeColor() {
+// onclick for button
+button.onclick = function(event) {
 
-    // generate 3 random numbers between 0 and 255
-    let num1 = Math.floor(Math.random() * 255);
-    let num2 = Math.floor(Math.random() * 255);
-    let num3 = Math.floor(Math.random() * 255);
+    event.preventDefault();
+    // create a div to contain the profile
+    let newDiv = document.createElement("div");
+    newDiv.style.border = "solid black 4px";
+    newDiv.style.display = "flex";
+    newDiv.style.alignItems = "center";
+    
+    document.body.appendChild(newDiv);
 
-    // change background color of colorDiv
-    colorDiv.style.backgroundColor = "rbg(" + num1 + "," + num2 + "," +num3 + ")";
+    // fetch data from the random user api
+    fetch("https://randomuser.me/api/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log("Sucess");
+            console.log(myJson.results[0].name.first);
 
-    // setTimer
-    setTimer(changeColor, 1000);
+            let newText = document.createElement("p");
+            newText.innerHTML = "My name is " + myJson.results[0].
+            name.first + " " + myJson.results[0].name.last + ". My email is: " + 
+            myJson.results[0].email + ". I am " + 
+            myJson.results[0].dob.age + " years old.";
+            newDiv.appendChild(newText);
+
+            let userImage = document.createElement("img");
+            userImage.src = myJson.results[0].picture.large;
+            newDiv.prepend(userImage);
+        })
+
+        // fetch data from the dog api
+        fetch("https://dog.ceo/api/breeds/image/random") 
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(myJson) {
+                console.log("Sucess! v2");
+                console.log(myJson);
+
+                let dogImage = document.createElement("img");
+                dogImage.src = myJson.message;
+                dogImage.style.height = "128px";
+                newDiv.appendChild(dogImage);
+            })
+        
 }
-// call the change color function
-changeColor();
